6d3c1a44d87dbf810e834efa1934ab0827e112c9
xuewenc <xuewenc@devvm613.frc0.facebook.com>
2020-05-14 23:38:00 -0700

    [JIT]  JIT should let people know we inferred an argument as a tensor
    Summary:
    
    This PR solves issue (#37200).
    Error is encountered during IR generation while trying to resolve the call to sum.
    Should let user know it inferred the value for argument 'dim' to be of type 'Tensor'
    because it was not annotated with an explicit type.
    
    Test Plan:
    
    Add code to reprodue the issue (#37200)
    `python test/test_jit.py TestJit.test_inferred_as_tensor`
    
    Reviewers:
    meghanl,suo
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 16b236bdee4df998f876e16065eb1a7c9f9e4b42
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/38527

--- torch/csrc/jit/frontend/script_type_parser.cpp
+++ torch/csrc/jit/frontend/script_type_parser.cpp
@@ -272,6 +272,9 @@ std::vector<Argument> ScriptTypeParser::parseArgsFromDecl(
       // If this param doesn't have a type, default to "tensor"
       is_inferred_type = true;
       type = TensorType::get();
+      if (auto pt = type->cast<TensorType>()) {
+        pt->setToInferredType();
+      }
     } else {
       // BroadcastList list can only appear at the argument level
       Expr type_expr = decl_arg.type().get();
